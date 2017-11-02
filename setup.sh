8af6effe45c1be3dfc878dae7098ca011eda53b7
Nicolas Andrade <nico@nmac.com.ar>
2017-11-02 17:50:38 -0400

    Adding bluetooth fix for pulseaudio / gnome / gdm

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get install aptitude build-essential curl synaptic git ;
 ## Terminator
 sudo add-apt-repository ppa:gnome-terminator;
 
-## Google chrome as per 
+## Google chrome as per
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
@@ -34,18 +34,35 @@ sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
 
 
 ## Laptop tools for performance and battery life as per http://linrunner.de/en/tlp/docs/tlp-linux-advanced-power-management.html#installation
-
 sudo add-apt-repository ppa:linrunner/tlp
 
+## VLC Daily build (might be unstable)
+sudo add-apt-repository ppa:videolan/stable-daily
+
+
+
+sudo aptitude update;
 sudo aptitude install terminator google-chrome-stable \
 atom htop mixxx gnome-tweak-tool \
 ubuntu-restricted-extras gimp ssh virtualbox knocker \
-cinelerra-cv clementine tlp acpi-call-dkms gtkpod
+cinelerra-cv clementine tlp acpi-call-dkms gtkpod vlc net-tools dos2unix \
+pwgen easytag
+
+
+## Install Hercules DJ Controller RMX Udev rule
+sudo wget https://raw.githubusercontent.com/mixxxdj/mixxx/master/res/linux/mixxx.usb.rules -O /etc/udev/rules.d/mixxx.usb.rules
+ME=`whoami`
+sudo usermod -aG users $ME
 
 ## Wallpaper changer
 git clone https://github.com/neffo/bing-wallpaper-gnome-extension.git $HOME/.local/share/gnome-shell/extensions/BingWallpaper@ineffable-gmail.com
 
 
 
-
-
+### Make it so GDM does not start pulseaudio as per https://www.debuntu.org/how-to-disable-pulseaudio-and-sound-in-gdm/ (for bluetooth pairing)
+sudo mkdir -p /var/lib/gdm/.pulse/
+sudo touch /var/lib/gdm3/.pulse/client.conf ; sudo chmod 777 /var/lib/gdm3/.pulse/client.conf
+echo "autospawn = no" > /var/lib/gdm3/.pulse/client.conf
+echo "daemon-binary = /bin/true" >> /var/lib/gdm3/.pulse/client.conf
+sudo chmod 640 /var/lib/gdm3/.pulse -R
+sudo chown gdm:gdm /var/lib/gdm3/.pulse  -R
